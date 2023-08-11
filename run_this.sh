cbd60599c805b945fae54a418232a15b9995319e
Scott Hosking <jshosking@gmail.com>
2023-08-11 07:06:18 +0100

    add Andersson and Parthipan 2023 papers

--- run_this.sh
+++ run_this.sh
@@ -2,8 +2,8 @@
 
 pybtex-convert jsh_bib.bib jsh_bib.yaml
 
-python yaml_pd.py --short_version False
-python yaml_pd.py --short_version True
+python3 yaml_pd.py --short_version False
+python3 yaml_pd.py --short_version True
 
 cp publications.html ../scotthosking.github.io/04_publications.html
 cp publications_short.html ../scotthosking.github.io/05_publications_short.html
